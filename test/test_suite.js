dbe509463e1da539a67f44b65ae4b21ac5e5784f
David Nguyen <david@Davids-MacBook-Air.local>
2022-04-29 10:43:30 +1000

    Initial commit

---
+++ test/test_suite.js
@@ -0,0 +1,9 @@
+
+
+//test test case
+describe('Example test', () => {
+    it('should just pass lol', async () => {
+        const test_math = 1+1
+        await expect(test_math).toEqual(2);
+    });
+});
