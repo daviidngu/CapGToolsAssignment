9cb2463bd8b3307b8f23d9723b8553f95f1f3749
David Nguyen <david@Davids-MacBook-Air.local>
2022-05-02 12:37:12 +1000

    Added API get request tests heheh :)

--- test/test_suite.js
+++ test/test_suite.js
@@ -1,9 +1,13 @@
 const user_details = require("./test_configs");
 const home_page = require("./page_objects/home_page");
 const signIn_page = require("./page_objects/signIn_page");
-
+const fetch = require('node-fetch');
+const assert = require('chai').assert
+const expect = require('chai').expect
+//import fetch from "node-fetch";
 
 //Login to Amazon
+/*
 describe('Log in to Amazon', () => {
     it('should login with valid credentials', async () => {
         await home_page.open_home_page();
@@ -15,5 +19,33 @@ describe('Log in to Amazon', () => {
     });
 });
 
+*/
 
-console.log(user_details)
+describe('API GET request', () => {
+    const API_Base_URL = "https://reqres.in/api/users"
+    
+    it('successful GET request with query param', async () => {
+        const page_num = 2
+        const query_param = "?page="+String(page_num)
+        const response = await fetch(API_Base_URL+query_param);
+        expect(response.status).to.equal(200);
+    });
+
+    it('successful GET request with path var', async () => {
+        const user_num = 2
+        const path_var = "/"+String(user_num)
+        const response = await fetch(API_Base_URL+path_var);
+        expect(response.status).to.equal(200);
+    });
+
+    it('fail GET request with path var', async () => {
+        const user_num = 100000
+        const path_var = "/"+String(user_num)
+        const response = await fetch(API_Base_URL+path_var);
+        expect(response.status).to.equal(404);
+    });
+
+
+
+    
+});
